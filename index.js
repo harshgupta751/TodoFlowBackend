4b02d842a5d77cd5b95a73cd376ce97ce34a10ea
harshgupta751 <harshachieve300@gmail.com>
2026-05-08 18:48:21 +0530

    CI-CD testing completed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.get('/health', (req, res)=>{
 
   res.status(200).json({
-    status: "He",
+    status: "ok",
     uptime: process.uptime(),
     timestamp: new Date(),
   });
